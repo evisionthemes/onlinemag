9c652f0ae992745bf07426787fc89ad1045d8c87
pradip <cloudpradip@gmail.com>
2017-07-20 11:40:42 +0545

    changes

--- inc/hooks/homepage-widget.php
+++ inc/hooks/homepage-widget.php
@@ -15,14 +15,14 @@ if ( ! function_exists( 'onlinemag_widget_section' ) ) :
             <div class="container">
                 <div class="row">
                     <!-- Main-panel Content -->
-                    <div class="col-xs-12 col-sm-12 col-md-8">
+                    <div class="col-xs-12 col-sm-12 col-md-8 home-main-content">
                         <div class="main-panel-section">
                             <!-- Main-panel Top Content -->
                                 <?php dynamic_sidebar( 'homepage-main-section' ); ?>
                         </div>
                     </div>
                     <!-- Sidebar Content -->
-                    <div class="col-xs-12 col-sm-12 col-md-4">
+                    <div class="col-xs-12 col-sm-12 col-md-4 sidebar-home">
                         <div class="sidebar-sections">
                             <!-- Sidebar Top Content -->
                             <?php get_sidebar(); ?>
